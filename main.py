0e8c9efbdeff4f9f77038d70a670381d5f20c151
Jean Cupe <jeancupe1@gmail.com>
2022-01-27 21:58:58 +0100

    Adding Heroku app configuration

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ import starter.config as config
 from starter.ml.data import process_data
 from starter.ml.model import inference
 
+import os
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
+
+
 app = FastAPI(
     title="API for salary predictor",
     description="This API helps to classify",
